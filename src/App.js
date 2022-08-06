64f7cf7dee3431a964af5366f09551e602442186
KarenJPerez1280 <kjohanamelo@gmail.com>
2022-08-06 12:41:24 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import dictionary from './dictionary.png';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <img src={dictionary} className="Dictionary" alt="img" />
+
+      </header>
+    </div>
+  );
+}
+
+
